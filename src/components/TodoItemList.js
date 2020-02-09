7eaf52e9971488d27933a062e458f7506a697fd8
DanielKim <dltcm0827@gmail.com>
2020-02-08 21:53:42 -0800

    add TodoItemList(Component)

---
+++ src/components/TodoItemList.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class TodoItemList extends Component {
+  render() {
+    const { todos, onToggle, onRemove } = this.props;
+
+    return <div></div>;
+  }
+}
+
+export default TodoItemList;
+
+/*
+TodoItemList(Component) will do
+render multiple TodoItem Component.
+Since made Template Component and did styling on it
+no need to add styling for TodoItemList(Component).
+
+In case of render a List, especially when list is dynamic(changes)
+need to make the component with class type to optimize performance.
+(https://reactjs.org/docs/optimizing-performance.html)
+
+TodoItemList(Component) will receive 3 propss
+1. todos: todo
+2. onToggle: function to toggle checkbox
+3. onRemove: function to delete item
+*/
