701c6697d95c9266a6cdc38ed99ac7065bd95ec7
DanielKim <dltcm0827@gmail.com>
2020-02-08 22:50:26 -0800

    added TodoItem(Component) && TodoItem.css

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 
 import TodoListTemplate from "./TodoListTemplate";
 import Form from "./Form";
+import TodoItemList from "./TodoItemList";
 
 class App extends Component {
   render() {
     return (
-      <TodoListTemplate form={<Form />}>Finished Template</TodoListTemplate>
+      <TodoListTemplate form={<Form />}>
+        <TodoItemList />
+      </TodoListTemplate>
     );
   }
 }
